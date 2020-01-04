301458436fc4b14bdcba3831c31cb5455330acdd
JackWalsh1 <jacksonwalsh931@gmail.com>
2020-01-04 01:02:25 -0500

    Added potential way to calculate GPA. Maybe. I'm sorry in advance.

--- src/com/apcsa/controller/Utils.java
+++ src/com/apcsa/controller/Utils.java
@@ -72,6 +72,101 @@ public class Utils {
         return response.equals("y");
     }
     
+    /**
+     * Sorts the list of students by rank, using the index to update the underlying class rank.
+     * 
+     * @param students the list of students
+     * @return the updated list of students
+     */
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static ArrayList<Student> updateRanks(ArrayList<Student> students) {
+        Collections.sort(students, new Comparator() {
+
+            // compares each student based on gpa to aid sorting
+            
+            @Override
+            public int compare(Object student1, Object student2) {
+                if (((Student) student1).getGpa() > ((Student) student2).getGpa()) {
+                    return -1;
+                } else if (((Student) student1).getGpa() == ((Student) student2).getGpa()) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+            
+        });
+        
+        // applies a class rank (provided the student has a measurable gpa)
+        
+        int rank = 1;
+        for (int i = 0; i < students.size(); i++) {
+            Student student = students.get(i);
+            
+            student.setClassRank(student.getGpa() != -1 ? rank++ : 0);
+        }
+                
+        return students;
+    }
+    
+    /**
+     * Computes a grade based on marking period grades and exam grades.
+     * 
+     * @param grades a list of grades
+     * @return the final grade
+     */
+
+    public static Double getGrade(Double[] grades) {
+        int mps = 0;
+        double mpSum = 0;
+        double mpAvg = -1;
+        double mpWeight = -1;
+
+        int exams = 0;
+        double examSum = 0;
+        double examAvg = -1;
+        double examWeight = -1;
+        
+        // compute sume of marking period and/or exam grades
+        
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] != null) {
+                if (i < 2 || (i > 2 && i < 5)) {        // marking period grade
+                    mps++;
+                    mpSum = mpSum + grades[i];
+                } else {                                // midterm or final exam grade
+                    exams++;
+                    examSum = examSum + grades[i];
+                }
+            }
+        }
+        
+        // compute weights and averages based on entered grades
+        
+        if (mps > 0 && exams > 0) {
+            mpAvg = mpSum / mps;
+            examAvg = examSum / exams;
+             
+            mpWeight = 0.8;
+            examWeight = 0.2;
+        } else if (mps > 0) {
+            mpAvg = mpSum / mps;
+            
+            mpWeight = 1.0;
+            examWeight = 0.0;
+        } else if (exams > 0) {
+            examAvg = examSum / exams;
+            
+            mpWeight = 0.0;
+            examWeight = 1.0;
+        } else {
+            return null;
+        }
+                                
+        return round(mpAvg * mpWeight + examAvg * examWeight, 2);
+    }
+    
 }
 
 
